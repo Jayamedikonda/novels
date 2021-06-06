8be0271af2a5b5f22eca71011707cb004ed26e82
Jayamedikonda <82174057+Jayamedikonda@users.noreply.github.com>
2021-06-06 21:45:11 +0530

    updated

--- novel.js
+++ novel.js
@@ -2,3 +2,19 @@ function my()
 {
     document.location='1.html';
 }
+function black()
+{
+    document.getElementById("cont").style.backgroundColor="black";
+    document.getElementById("cont").style.color="white";
+    
+    
+}
+function white()
+{
+    document.getElementById("cont").style.backgroundColor="white";
+    document.getElementById("cont").style.color="black";
+    document.getElementById("heade").style.backgroundColor="black";
+    document.getElementById("heade").style.color="white";
+    
+      
+}
